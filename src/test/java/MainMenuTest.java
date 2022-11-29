2b5c8971490ccadf15bace6e60d76140ef1522ea
Khalil <khalil.hersi@hotmail.co.uk>
2022-11-29 09:43:43 +0000

    completed test for addFlight()

--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainMenuTest {
     Flight flight1;
@@ -12,14 +13,16 @@ public class MainMenuTest {
         ArrayList<String> passengers = new ArrayList<String>();
         passengers.add(0, "Bob");
         flight1 = new Flight("Singapore", 43, passengers);
-//        ArrayList<Flight> flights = new ArrayList<>();
+
+//      ArrayList<Flight> flights = new ArrayList<>();
 
     }
+
 
     @Test
     public void testAddFlight(){
         MainMenu.flights.add(flight1);
-        assertThat
+        assertThat(MainMenu.flights.contains(flight1)).isEqualTo(MainMenu.flights.add(flight1));
     }
 
 
